86e928d61473fde922b7369c72db1428d3836743
DLC <shabrov23.02@gmail.com>
2023-07-02 00:07:45 +0300

    added mutex for mock

--- internal/storage/mockStor.go
+++ internal/storage/mockStor.go
@@ -16,10 +16,14 @@ type TestStore struct {
 }
 
 func (m TestStore) createStor(ctx context.Context, cfg *config.ServerConfig) (UserStorage, error) {
+	m.Mutex.Lock()
+	m.Mutex.Unlock()
 	return mockS, nil
+
 }
 
 func (m TestStore) createUser(ctx context.Context, u *model.UserInfo) error {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, u)
 	var r0 error
 	if ret != nil {
@@ -28,15 +32,17 @@ func (m TestStore) createUser(ctx context.Context, u *model.UserInfo) error {
 			if u.Login == new.Login {
 				return apperrors.NewConflict("username with login: " + u.Login)
 			}
+			m.Mutex.Unlock()
 			return nil
 
 		}
 	}
-
+	m.Mutex.Unlock()
 	return r0
 }
 
 func (m TestStore) findByLogin(ctx context.Context, login *string) (*model.UserInfo, error) {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, login)
 
 	var r0 *model.UserInfo
@@ -49,10 +55,11 @@ func (m TestStore) findByLogin(ctx context.Context, login *string) (*model.UserI
 	if ret.Get(1) != nil {
 		r1 = ret.Get(1).(error)
 	}
-
+	m.Mutex.Unlock()
 	return r0, r1
 }
 func (m TestStore) getAllOrdersByLogin(ctx context.Context, login *string) (*model.User, error) {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, login)
 
 	var r0 *model.User
@@ -62,11 +69,12 @@ func (m TestStore) getAllOrdersByLogin(ctx context.Context, login *string) (*mod
 	if len(r0.Orders) == 0 {
 		return r0, apperrors.NewNoContent()
 	}
-
+	m.Mutex.Unlock()
 	return r0, nil
 }
 
-func (m TestStore) addNewOrder(ctx context.Context, u *model.User) error {
+func (m TestStore) addNewOrder(ctx context.Context, u *model.User) error {\
+	m.Mutex.Lock()
 	ret := m.Called(ctx, u)
 
 	var r0 *model.User
@@ -81,11 +89,12 @@ func (m TestStore) addNewOrder(ctx context.Context, u *model.User) error {
 	} else if r0.Info.Login != u.Info.Login && r0.Orders[0].ID == u.Orders[0].ID {
 		r1 = apperrors.NewConflict("")
 	}
-
+	m.Mutex.Unlock()
 	return r1
 }
 
 func (m TestStore) getBalanceWithdraw(ctx context.Context, login *string) (*model.BalanceResp, error) {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, login)
 
 	var r0 *model.BalanceResp
@@ -98,22 +107,24 @@ func (m TestStore) getBalanceWithdraw(ctx context.Context, login *string) (*mode
 	if ret.Get(1) != nil {
 		r1 = ret.Get(1).(error)
 	}
-
+	m.Mutex.Unlock()
 	return r0, r1
 }
 
 func (m TestStore) getUBalance(ctx context.Context, login *string) (float64, error) {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, login)
 
 	var r0 float64
 	if ret.Get(0) != nil {
 		r0 = ret.Get(0).(float64)
 	}
-
+	m.Mutex.Unlock()
 	return r0, nil
 }
 
 func (m TestStore) getAllWithdrawsByLogin(ctx context.Context, login *string) (*model.User, error) {
+	m.Mutex.Lock()
 	ret := m.Called(ctx, login)
 
 	var r0 *model.User
@@ -121,14 +132,17 @@ func (m TestStore) getAllWithdrawsByLogin(ctx context.Context, login *string) (*
 		r0 = ret.Get(0).(*model.User)
 	}
 	if len(r0.Withdraws) == 0 {
+		m.Mutex.Unlock()
 		return r0, apperrors.NewNoContent()
 	}
-
+	m.Mutex.Unlock()
 	return r0, nil
 
 }
 
 func (m TestStore) addNewOderWithdraw(ctx context.Context, u *model.User) error {
+	m.Mutex.Lock()
+	m.Mutex.Unlock()
 	return apperrors.NewAccepted()
 }
 
